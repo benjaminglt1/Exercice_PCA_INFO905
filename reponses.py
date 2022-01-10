d0880bbe1f7b8e91174dff4a1812a65ade0693ce
Benjamin Gallet <benjamin.gallet@api-k.com>
2022-01-10 16:28:20 +0100

    [doc] documentation

---
+++ reponses.py
@@ -0,0 +1,5 @@
+def q1():
+    print('features = ["highway-mpg","engine-size","horsepower","curb-weight",]\n')
+
+def q2():
+    print('Pour standardiser notre jeu de données :\nX = (X - X.mean(axis=0)) / X.std(axis=0)')
